0fd37008cd9c00e37968a6becb1cd4ca4db60efa
Andreas <hamburger1984@gmail.com>
2022-01-06 22:07:27 +0100

    Begin dotnet core C# implementation

---
+++ impls/dotnet-cs/step0_repl.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string READ(string input) { return input; }
+
+string EVAL(string input) { return input; }
+
+string PRINT(string input) { return input; }
+
+string rep(string input) {
+  var read = READ(input);
+  var eval = EVAL(read);
+  return PRINT(eval);
+}
+
+
+while(true){
+  Console.Write("user> ");
+  var input = Console.ReadLine();
+
+  if(input==null) break;
+
+  Console.WriteLine(rep(input));
+}
